c66d25eec6a8006a800b95ff37caa6538d5b08a8
harry <harry@iacryl.com>
2020-06-23 14:18:53 +0900

    chapter 3

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import post_list
+from .views import post_list, post_detail, post_new, post_edit
 
 urlpatterns = [
     path('', post_list, name='post_list'),
+    path('post/<int:pk>/', post_detail, name='post_detail'),
+    path('post/new/', post_new, name='post_new'),
+    path('post/<int:pk>/edit/', post_edit, name='post_edit'),
 ]
